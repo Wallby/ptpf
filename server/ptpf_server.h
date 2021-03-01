a3c8615b602c4a55d41bee8161189e78d507a2df
Wallby <maartentenvelden@hotmail.com>
2021-03-01 16:08:03 +0100

    + added ptpf_server.h and ptpf_server.cpp + added ptpf_loader.h and ptpf_loader.c

---
+++ server/ptpf_server.h
@@ -0,0 +1,11 @@
+#ifndef PTPF_SERVER_H
+#define PTPF_SERVER_H
+
+// "fputs format" used by ptpf
+// (error:|warning:) <description>
+
+// NOTE: returns 0 if "something went wrong"
+//       returns 1 if all is ok
+//extern int ptpf_main(int argc, char** argv);
+
+#endif
